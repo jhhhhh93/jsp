5a8cf93dcd3ee00244c8507810153b139d1e650c
jhhhhh93 <parkjinha93@gmail.com>
2019-09-05 09:37:57 +0900

    국제화(타임존) fmt : setTimeZone 특정 시간대의 현재 시간을 출력(방콕)
    el function
     .fnFunction.jsp

--- src/main/java/kr/or/ddit/servlet/TimeZoneController.java
+++ src/main/java/kr/or/ddit/servlet/TimeZoneController.java
@@ -1,28 +1,28 @@
 package kr.or.ddit.servlet;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.TimeZone;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * Servlet implementation class SelectLocaleController
+ * Servlet implementation class TimeZoneController
  */
-@WebServlet("/selectLocale")
-public class SelectLocaleController extends HttpServlet {
+@WebServlet("/timeZone")
+public class TimeZoneController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String locale = request.getParameter("locale");
-		locale = locale == null ? "ko" : locale;
-		
-		request.setAttribute("locale", locale);
-		request.getRequestDispatcher("/jstl/selectLocale.jsp").forward(request, response);
+		String[] timeZoneList = TimeZone.getAvailableIDs();
+		request.setAttribute("timeZoneList", timeZoneList);
+		request.setAttribute("dt", new Date());
+		request.getRequestDispatcher("/jstl/timeZone.jsp").forward(request, response);
 	}
+
 
 }
